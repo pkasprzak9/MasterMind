75b307b303591b01bd44caab9424c4a691fc85bd
pkasprzak9 <pawkasprzak9@gmail.com>
2023-05-11 19:29:42 +0200

    Create a method that plays game 12 times or until  player guess correct

--- game.rb
+++ game.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: false
+require 'pry-byebug'
 
 class Game
+  def initialize
+    @game_over = false
+    @count_rounds = 1
+  end
+
+  def play_game(codemaker, codebreaker)
+    codemaker.generate_code
+    puts codemaker.code.join('')
+
+    until @game_over || @count_rounds > 12
+      puts "Round: #{@count_rounds}"
+      play_round(codemaker, codebreaker)
+      @count_rounds += 1
+    end
+  end
+
+  private
+
   def play_round(codemaker, codebreaker)
-    codemaker.code == codebreaker.code
+    codebreaker.generate_code
+    @game_over = codemaker.code == codebreaker.code
   end
 end
